164fc0cd62b9d8415471ed2bf323fea38b63a154
daniqs22 <quesadadanielasoto@gmail.com>
2022-08-29 18:47:13 -0600

    mostrar minterminos

--- proyecto.py
+++ proyecto.py
@@ -1,3 +1,18 @@
+def muestra_primerosGrupos(grupos):
+    #Comenzamos la impresión de la agrupación primaria
+    print("\n\n\n\nNúmero de Gpo.\tMintérminos\t\tExpresión en BCD")
+    print ('='*60)
+    for i in sorted(grupos.keys()):
+        print("%5d:"%i) # Prints group number
+        for j in grupos[i]:
+            print("\t\t    %-20d%s"%(int(j,2),j)) # Imprime los mintérminos y su representación binaria (BCD)
+        print('-'*60)
+    #Término de la impresión de la agrupación primara
+
+
+
+
+
 print('Por favor ingrese los términos separados por un espacio')
 mt = [int(i) for i in input("Ingrese los mintérminos ").strip().split()]
 mt.sort()
@@ -17,4 +32,4 @@ for minterm in minterminos:
         grupos[bin(minterm).count('1')] = [bin(minterm)[2:].zfill(size)] #Si no se puede agregar se crea un nuevo key con los grupos
 # Término de la agrupación primaria
 # grupos = {cantidad de 1s: [grupos que solo tengan esa cantidad]}
-print (grupos)
+muestra_primerosGrupos(grupos)
